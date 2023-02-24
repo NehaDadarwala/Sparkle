d8fbdc9ccac129ed954d80f3052f5559ba062dd5
sakshi-124 <vaidyasakshi434@gmail.com>
2023-02-24 11:15:08 -0400

    changes in inventory module

--- src/Components/StockCardView.js
+++ src/Components/StockCardView.js
@@ -6,36 +6,44 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import ring from '../ring.png'
+import { useNavigate } from "react-router-dom";
+import { Grid } from '@mui/material';
 
 const StockCardView = (props) => {
+    let navigate = useNavigate();
+    const navigateToNewPage = () => {
+        let path = '/modifyStock';
+        navigate(path);
+    }
+
     const stock = props.stock.map((stock) => {
-    return (
-        <div className='App'>
-            <Card sx={{ maxWidth: 345 , margin:10}}>
-                <CardMedia
-                    image={ring}
-                    sx={{ height: 300 }}
-                    title="Ring"
-                />
-                <CardContent  align='left'>
-                    <Typography gutterBottom variant="h5" component="div">  
-                        {stock.productName}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" >
-                       Qty : {stock.qty}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                       Price : {stock.price}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Modify</Button>
-                </CardActions>
-            </Card>
-        </div>
-    )
-});
-return <div>{stock}</div>;
+        return (
+            <div className='App'>
+                <Card sx={{ maxWidth: 345, margin: 10 }}>
+                    <CardMedia
+                        image={ring}
+                        sx={{ height: 300 }}
+                        title="Ring"
+                    />
+                    <CardContent align='left'>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {stock.productName}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" >
+                            Qty : {stock.qty}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Price : {stock.price}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" onClick={navigateToNewPage}>Modify</Button>
+                    </CardActions>
+                </Card>
+            </div>
+        )
+    });
+    return <Grid > <Grid item md={4} > {stock} </Grid></Grid>;
 };
 
 export default StockCardView
