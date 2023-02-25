d247aa077a22413f6fa279d846380f0a4a319ee3
sakshi-124 <vaidyasakshi434@gmail.com>
2023-02-25 19:55:30 -0400

    validations added to modify stock

--- src/Pages/Inventory/ModifyStock.js
+++ src/Pages/Inventory/ModifyStock.js
@@ -1,27 +1,53 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import Textarea from '@mui/material/TextareaAutosize';
 import Container from '@mui/material/Container';
-import { Autocomplete } from '@mui/material';
-import CustomButton from '../../Components/CustomButton';
+import { Autocomplete, Grid } from '@mui/material';
+import Button from '@mui/material/Button';
+//import CustomButton from '../../Components/CustomButton';
+//import { useCallback, useRef } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+//import { useNavigate } from 'react-router-dom';
 
+function ModifyStock() {
 
-function modifyStock() {
+    const productRefNumber = [
+        { label: '#RING01' },
+        { label: '#EARRING01' },
+        { label: '#NECKLACE01' }
+    ];
 
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     const data = new FormData(event.currentTarget);
-    //     console.log({
-    //         email: data.get('email'),
-    //         password: data.get('password'),
-    //     })
-    // };
+    const productNameuseRef = React.useRef(null);
+    const categoryuseRef = React.useRef(null);
+    const qtyuseRef = React.useRef(null);
+    const priceuseRef = React.useRef(null);
+    const descriptionuseRef = React.useRef(null);
+    const addButtonuseRef = React.useRef(null);
 
+    const defaultValues = {
+        productName: '',
+        category: '',
+        qty: '',
+        price: '',
+        description: '',
+    };
+
+    const [formValues, setFormValues] = useState(defaultValues)
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        console.log("name = " + name);
+        console.log("value" + value);
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        });
+    };
 
     return (
-       
-       
         <div>
             <div>
                 <Container component="main" maxWidth="xs">
@@ -31,84 +57,197 @@ function modifyStock() {
                             marginTop: 8,
                             display: 'flex',
                             flexDirection: 'column',
-                            alignItems: 'center',
+                            alignItems: 'center'
                         }}
                     >
                         {/* <Typography component="h1" variant="h5">
                             Modify Stock
                         </Typography> */}
-                        {/* onSubmit={handleSubmit} */}
-                        <Box component="form" noValidate sx={{ mt: 1 }}>
+
+                        <Box component="form" onSubmit={handleSubmit(console.log)} sx={{ mt: 1 }} >
+
                             <Autocomplete
                                 disablePortal
-                                id="combo-box-demo"
-                                autoFocus
-                                // options={top100Films}
-                                // sx={{ width: 300 }}
+                                id="productRefNumber"
+                                options={productRefNumber}
                                 fullWidth
-                                renderInput={(params) => <TextField {...params} label="Search Product" />}
+                                renderInput={(params) => <TextField {...params} label="Product Reference Number" />}
                             />
                             <TextField
+                                inputProps={{
+                                    onKeyPress: event => {
+                                        const { key } = event;
+                                        console.log(key);
+                                        if (key === "Enter") {
+                                            categoryuseRef.current.focus();
+                                        }
+                                    }
+                                }}
+                                inputRef={productNameuseRef}
                                 margin="normal"
-                                required
                                 fullWidth
-                                id="productName"
                                 label="Product Name"
                                 name="productName"
+                                value={formValues.productName}
+                                {...register("productName", {
+                                    onChange: (e) => { handleInputChange(e) },
+                                    required: "Product Name is required.",
+                                    pattern: {
+                                        message: "Product Name is required"
+                                    }
+                                })}
+                                error={Boolean(errors.productName)}
+                                helperText={errors.productName?.message}
+
                             />
                             <TextField
+                                inputProps={{
+                                    onKeyPress: event => {
+                                        const { key } = event;
+                                        console.log(key);
+                                        if (key === "Enter") {
+                                            qtyuseRef.current.focus();
+                                        }
+                                    }
+                                }}
+
+                                inputRef={categoryuseRef}
+                                //ref={focusNextRef}
                                 margin="normal"
-                                required
                                 fullWidth
-                                name="productCategory"
+                                name="category"
                                 label="Category"
-                                id="productCategory"
+                                value={formValues.category}
+                                {...register("category", {
+                                    onChange: (e) => { handleInputChange(e) },
+                                    required: "Category is required.",
+                                    pattern: {
+                                        message: "Category is required"
+                                    }
+                                })}
+                                error={Boolean(errors.category)}
+                                helperText={errors.category?.message}
                             />
                             <TextField
+                                inputProps={{
+                                    onKeyPress: event => {
+                                        const { key } = event;
+                                        console.log(key);
+                                        if (key === "Enter") {
+                                            priceuseRef.current.focus();
+                                        }
+                                    }
+                                }}
+                                inputRef={qtyuseRef}
                                 margin="normal"
-                                required
                                 fullWidth
-                                name="produtQty"
+                                name="qty"
                                 label="Qty"
                                 id="productQty"
+                                {...register("qty", {
+                                    onChange: (e) => { handleInputChange(e) },
+                                    required: "Quantity is required",
+                                    pattern: {
+                                        value: /^\d+$/,
+                                        message: "Should be number"
+                                    }
+                                })}
+                                error={Boolean(errors.qty)}
+                                helperText={errors.qty?.message}
                             />
                             <TextField
+                                inputProps={{
+                                    onKeyPress: event => {
+                                        const { key } = event;
+                                        console.log(key);
+                                        if (key === "Enter") {
+                                            descriptionuseRef.current.focus();
+                                        }
+                                    }
+                                }}
+                                inputRef={priceuseRef}
                                 margin="normal"
-                                required
                                 fullWidth
-                                name="productPrice"
+                                name="price"
                                 label="Price"
                                 id="productPrice"
+                                {...register("price", {
+                                    onChange: (e) => { handleInputChange(e) },
+                                    required: "Price is required",
+                                    pattern: {
+                                        value: /^\d+$/,
+                                        message: "Should be number"
+                                    }
+                                })}
+                                error={Boolean(errors.price)}
+                                helperText={errors.price?.message}
                             />
-                            <Textarea margin="normal"
-                                required
-                                name="productDescription"
-                                placeholder="Decription"
-                                id="productDescription"
-                                style={{ width: 500, marginTop: "3%" }}
-                                minRows={3}
+                            <TextField
+                                inputProps={{
+                                    onKeyPress: event => {
+                                        const { key } = event;
+                                        console.log(key);
+                                        if (key === "Enter") {
+                                            categoryuseRef.current.focus();
+                                        }
+                                    }
+                                }}
+                                inputRef={descriptionuseRef}
+                                margin="normal"
+                                fullWidth
+                                label="Description"
+                                name="description"
+                                value={formValues.description}
+                                {...register("description", {
+                                    onChange: (e) => { handleInputChange(e) },
+                                    required: "Description is required.",
+                                    pattern: {
+                                        message: "Description is required"
+                                    }
+                                })}
+                                error={Boolean(errors.description)}
+                                helperText={errors.description?.message}
+
                             />
-                            <div>
-                                <div>
+                            {/* <Grid item>
                                     <CustomButton
+                                        //ref={focusNextRef}
                                         label="Modify"
                                         type="submit"
                                     />
-                                </div>
-                                <div>
+                          
                                     <CustomButton
+                                        //ref={focusNextRef}
                                         label="Add"
                                         type="submit"
+                                        
                                     />
-                                </div>
-                            </div>
+                               
+                                </Grid> */}
+                            <Grid item>
+                                <Button style={{
+                                    margin: "20px", backgroundColor: '#444454',
+                                    color: '#bab79d', borderColor: '#b28faa', height: 50, width: 150,
+                                    borderRadius: 7
+                                }} variant="contained" type="submit"
+                                    ref={addButtonuseRef}>
+                                    Add
+                                </Button>
+                                <Button style={{
+                                    margin: "20px", backgroundColor: '#444454',
+                                    color: '#bab79d', borderColor: '#b28faa', height: 50, width: 150,
+                                    borderRadius: 7
+                                }} variant="contained" type="submit">
+                                    Modify
+                                </Button>
+                            </Grid>
                         </Box>
                     </Box>
                 </Container>
             </div>
         </div>
-       
+
     )
 }
 
-export default modifyStock
+export default ModifyStock
