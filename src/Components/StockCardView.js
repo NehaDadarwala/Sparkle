45ae1b00ffd55011228b1748d6d875208d6bbf21
sakshi-124 <vaidyasakshi434@gmail.com>
2023-02-25 22:02:03 -0400

    changes for responsive ui

--- src/Components/StockCardView.js
+++ src/Components/StockCardView.js
@@ -8,6 +8,7 @@ import Typography from '@mui/material/Typography';
 import ring from '../ring.png'
 import { useNavigate } from "react-router-dom";
 import { Grid } from '@mui/material';
+import { Container } from '@mui/system';
 
 const StockCardView = (props) => {
     let navigate = useNavigate();
@@ -18,6 +19,7 @@ const StockCardView = (props) => {
 
     return (
         <div>
+            <Container>
            <Grid
               container
               spacing={3}
@@ -26,8 +28,8 @@ const StockCardView = (props) => {
             >
              {props.stock.map((stock) => {
                return (
-                <Grid item xs={6} sm={4} md={4}>
-                  <Card sx={{ maxWidth: 345, margin: 10 }}>
+                <Grid item xs={12} sm={6} md={4}>
+                  <Card sx={{ maxWidth: 345, margin: 5 }}>
                     <CardMedia
                         image={ring}
                         sx={{ height: 300 }}
@@ -52,6 +54,7 @@ const StockCardView = (props) => {
             );
           })}
          </Grid>
+         </Container>
         </div>
       );
     //return <div>{stock}</div> ;
