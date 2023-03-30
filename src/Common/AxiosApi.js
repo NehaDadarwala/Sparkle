e0210ef3c67d9ed8a1f758951a7a6468f65e02cc
sakshi-124 <vaidyasakshi434@gmail.com>
2023-03-30 18:12:42 -0300

    Axios Api

--- src/Common/AxiosApi.js
+++ src/Common/AxiosApi.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const axiosApi = axios.create({
   baseURL: "https://sparkle-api.onrender.com",
+  //baseURL: "http://localhost:3000/",
+
 });
 
 export default axiosApi;
